128c868f9a5bb59b167c63d03e4d7da4fa3b33d1
Joe <joe@iachieved.it>
2016-01-01 12:03:58 -0600

    Dependency on Tide in github

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name:  "swiftysockets",
   dependencies: [
-    .Package(url:  "../Tide", majorVersion: 0),
+    .Package(url:  "https://github.com/iachievedit/Tide", majorVersion: 0),
   ]
 )
